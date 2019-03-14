27ef4d9a7bb70114ca8a1808477a8c662b96742c
Gootjes <`37846483+Gootjes@users.noreply.github.com`>
2019-03-14 13:54:34 +0100

    v0.2.1

--- tests/testthat/test.bookmark.R
+++ tests/testthat/test.bookmark.R
@@ -2,7 +2,7 @@ context("Testing output of bookmark hooks")
 
 test_that(desc = "Inline code", code = {
   expect_equal(
-    object = inline("Inline code", list()),
+    object = neverpaste_knitr_inline("Inline code", list()),
     expected = '`<w:bookmarkStart w:id=\"1\" w:name=\"unnamed-bookmark-1\" />`{=openxml}Inline code`<w:bookmarkEnd w:id=\"1\" />`{=openxml}'
     )
 })
