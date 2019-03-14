d821e867961a20a930f24e19a0ce15cea045df7d
F.C. Gootjes <P275309@workspace.rug.nl>
2019-03-14 13:51:49 +0100

    v0.2.1

--- R/bookmarks.R
+++ R/bookmarks.R
@@ -1,4 +1,66 @@
 
+sanitize_name <- function(x) {
+  stringr::str_replace_all(string = x, pattern = "[^a-zA-Z0-9_]+", replacement = "_")
+}
+
+wrap <- function(x, name, id) {
+  before <- paste0('`<w:bookmarkStart w:id="', id,'" w:name="', name,'" />`{=openxml}')
+  after <- paste0('`<w:bookmarkEnd w:id="', id,'" />`{=openxml}')
+
+  paste(before, x, after, sep = "")
+}
+
+#' @title Produce a bookmark
+#'
+#' @param x The object to be bookmarked
+#' @param name If NULL (default), a name is deducted from `x`
+#'
+#' @export
+bookmark <- function(x, name = NULL) {
+
+  if(is.null(name)) {
+    args <- as.list(match.call())
+    name <- names(args)$x
+  }
+
+  name <- sanitize_name(name)
+
+  i <- bookmark_counter$next_value()
+
+  knitr::asis_output(wrap(x = x, name = name, id = i))
+
+}
+
+#' @title Produce bookmarks
+#'
+#' @param ... Named arguments that you want to be bookmarked. If no name exists, it will be given a noname name.
+#'
+#' @return The bookmarked values pasted together.
+#'
+#' @export
+bookmarks <- function(...) {
+  args <- list(...)
+  bookmarks <- names(args)
+
+  bms <- Map(f = function(name){
+
+    output <- args[[name]]
+
+    i <- bookmark_counter$next_value()
+    if(nchar(name) == 0) {
+      n <- make_name(i)
+    } else {
+      n <- name
+    }
+
+    wrap(x = output, name = n, id = i)
+
+  }, names(args))
+
+  knitr::asis_output(paste(bms, collapse = ""))
+
+}
+
 bookmark_counter <- (function() {
 
   .initial <- 0
@@ -29,18 +91,7 @@ make_name <- function(count) {
   paste0("unnamed-bookmark-", count, sep = "", collapse = "")
 }
 
-bookmark_inline <- function(output, options) {
-  if(before) {
-    i <- bookmark_counter$next_value()
-    n <- make_name(i)
-    paste0('`<w:bookmarkStart w:id="', i,'" w:name="', n,'" />`{=openxml}')
-  } else {
-    i <- bookmark_counter$.get()
-    paste0('`<w:bookmarkEnd w:id="', i,'" />`{=openxml}')
-  }
-}
-
-bookmark <- function(output, options) {
+neverpaste_knitr_bookmark <- function(before, after, options) {
   if(before) {
     i <- bookmark_counter$next_value()
 
@@ -55,7 +106,7 @@ bookmark <- function(output, options) {
   }
 }
 
-inline <- function(output, options) {
+neverpaste_knitr_inline <- function(output, options) {
   i <- bookmark_counter$next_value()
   n <- make_name(i)
 
@@ -65,7 +116,7 @@ inline <- function(output, options) {
   paste0(before, output, after)
 }
 
-chunk <- function(output, options) {
+neverpaste_knitr_chunk <- function(output, options) {
 
   i <- bookmark_counter$next_value()
   n <- options$label
@@ -89,61 +140,11 @@ setup_knitr_bookmark_hooks <- function() {
 
   bookmark_counter$.reset()
 
-  knitr::knit_hooks$set(bookmark_inline = bookmark_inline)
+  knitr::knit_hooks$set(bookmark = neverpaste_knitr_bookmark)
 
-  knitr::knit_hooks$set(bookmark = bookmark)
+  knitr::knit_hooks$set(inline = neverpaste_knitr_inline)
 
-  knitr::knit_hooks$set(inline = inline)
-
-  knitr::knit_hooks$set(chunk = chunk)
+  knitr::knit_hooks$set(chunk = neverpaste_knitr_chunk)
 
 }
-#
-# bookmark_options <- list(
-#   bookmark_id = -1
-# )
-#
-# bookmark <- function(x, name = NULL) {
-# #   UseMethod("bookmark", x)
-# # }
-# #
-# # bookmark.character <- function() {
-#   if(is.null(name)) {
-#     name <- as.character(as.list(match.call())$x)
-#   }
-#
-#   bookmark_options$bookmark_id <- bookmark_options$bookmark_id + 1
-#
-#   structure(list(x = x, id = bookmark_options$bookmark_id, name = name), class = "bookmark")
-# }
-#
-# knit_print.bookmark <- function(x, ...) {
-#
-#   UseMethod("knit_print.bookmark", x$x)
-#
-# }
-#
-# knit_print.bookmark.default <- function(x, ...) {
-#   r <- x
-#   knitr::knit_print(knitr::asis_output(paste0("`<w:bookmarkStart w:id=\"",x$id,"\" w:name=\"",x$name,"\" />`{=openxml}",r,"`<w:bookmarkEnd w:id=\"",x$id,"\" />`{=openxml}")))
-# }
-#
-# knit_print.bookmark.flextable <- function(x, ...) {
-#
-#   r <- knitr::knit_print(x$x)
-#
-#
-#   knitr::knit_print(knitr::asis_output(paste0("`<w:bookmarkStart w:id=\"",x$id,"\" w:name=\"",x$name,"\" />`{=openxml}",r,"`<w:bookmarkEnd w:id=\"",x$id,"\" />`{=openxml}")))
-# }
-#
-# print_bookmark <- function(x, name = NULL) {
-#   if(is.null(name)) {
-#     name <- as.character(as.list(match.call())$x)
-#   }
-#
-#   bookmark_options$bookmark_id <- bookmark_options$bookmark_id + 1
-#
-#   knitr::knit_print(knitr::asis_output(paste0("`<w:bookmarkStart w:id=\"",bookmark_options$bookmark_id,"\" w:name=\"",name,"\" />`{=openxml}")))
-#   knitr::knit_print(x)
-#   knitr::knit_print(knitr::asis_output(paste0("`<w:bookmarkEnd w:id=\"",bookmark_options$bookmark_id,"\" />`{=openxml}")))
-# }
+
